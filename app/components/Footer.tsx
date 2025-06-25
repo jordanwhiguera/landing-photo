6e0d7d911c245a5daa6bee955614e98a313381b6
Jordan Higuera <jordanwhiguera@gmail.com>
2025-06-25 15:46:10 -0400

    padd fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import Container from "./Container";
 
 const Footer = () => {
   return (
-    <Container className=" my-12 lg:my-16 bg-footer flex flex-col md:flex-row justify-between items-center text-black bg-[#e3d6c3;]">
+    <Container className=" py-12 lg:py-16 bg-footer flex flex-col md:flex-row justify-between items-center text-black bg-[#e3d6c3;]">
       <div className="">
         <span className=" font-semibold">
           Copyright © 2024 - Open Home Media LLC
